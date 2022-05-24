b164c2908735642482289a18250f22a55d986b1d
Amit Hasan Sadhin <71214071+samithasan7866@users.noreply.github.com>
2022-05-24 13:02:52 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:multi_store_app/main_screens/customer_home.dart';
+import 'package:multi_store_app/main_screens/supplier_home.dart';
+import 'package:multi_store_app/main_screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/welcome_screen',
+      routes: {
+        '/welcome_screen': (context) => const WelcomeScreen(),
+        '/customer_home': (context) => const CustomerHomeScreen(),
+        '/supplier_home': (context) => const SupplierHomeScreen(),
+      },
+    );
+  }
+}
